3a86927324c7ae1e3d1042e802915f2751c933b1
NoginaDaria <nogina_daria@mail.ru>
2020-05-05 22:44:45 +0300

    Update vectorizer.py

--- vectorizer.py
+++ vectorizer.py
@@ -5,13 +5,34 @@ with open('page_dict.p', 'rb') as fp:
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-vectorizer = TfidfVectorizer(min_df=0.02, max_df=0.1, stop_words='english')
-vectors = vectorizer.fit_transform(page_dict.values()).toarray()
-vector_map = dict(zip(page_dict.keys(), vectors.tolist()))
+all_vectors = []
+for group in train_data.group_id.unique():
+    texts = []
+    for i in train_data[train_data['group_id'] == group].doc_id:
+        texts.append(page_dict.get(i))
+    vectorizer = TfidfVectorizer(min_df = 0.01, max_df=0.1)
+    vectors = vectorizer.fit_transform(texts)
+    print(vectors.shape)
+    all_vectors.append(vectors)
+    
 import pickle
 with open('vector_map.p', 'wb') as fp:
     pickle.dump(vector_map, fp)
     
+#UMAP
+import umap
+import seaborn as sns
+    
+a = 5
+mapper = umap.UMAP(metric='cosine')
+umap_res = mapper.fit_transform(all_vectors[a])
+umap_res = pd.DataFrame(umap_res)
+umap_res['target'] = list(train_data[train_data['group_id'] == a+1]['target'])
+    
+sns.scatterplot(data = pd.DataFrame(umap_res), x=0, y=1, hue='target')    
+    
+    
+ # не знаю, не правила, пусть будет...
 import pandas as pd
 train_data = pd.read_csv('./anomaly-detection-competition-ml1-ts-spring-2020/train_groups.csv')
 traingroups_titledata = {}
