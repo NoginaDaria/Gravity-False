b6c07d02daa45f64e31f4966dc1f508855d67b44
NoginaDaria <nogina_daria@mail.ru>
2020-05-22 19:22:34 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,6 +7,9 @@ from sklearn.metrics import pairwise_distances
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import f1_score
 from sklearn.model_selection import GroupKFold
+from tqdm import tqdm_notebook
+from scipy.stats import rankdata
+from itertools import product
 import enchant
 import numpy as np
 import pandas as pd
@@ -24,13 +27,13 @@ tokenizer = RegexpTokenizer(r'\w+')
 
 def preprocess_text(text):
     text = text.lower()
-    text = ' '.join(tokenizer.tokenize(text))
+    #text = ' '.join(tokenizer.tokenize(text))
     tokens = [mystem.stem(mystem_e.stem(token)) for token in re.sub('[^a-zа-я0-9]', ' ', text).split() 
                  if not token in russian_stopwords \
                  and not token in english_stopwords \
-                 and not token.isdigit() \
+                 #and not token.isdigit() \
                  and token.strip() not in punctuation \
-                 and not token in ['ru', 'com'] \
+                 #and not token in ['ru', 'com'] \
                  #and len(token) > 1 \
              ]
     return ' '.join(tokens)
@@ -63,13 +66,25 @@ def cosine(group, n_features, vectors):
         X[i] = sorted(title, )[1:n_features + 1]     
     return X
 
+def cosine_add_feat(group, n_features, vectors):
+    X = np.empty(shape=(group.size, n_features*2+104))
+    dist = pairwise_distances(vectors[group], metric='cosine')
+    dist_ranks = rankdata(dist).reshape(dist.shape)
+    for i, title in enumerate(dist):
+        m = sorted(title, )[1:n_features + 1] 
+        m_ranks = sorted(dist_ranks[i])[1:n_features + 1] 
+        X[i] = np.concatenate((m, m_ranks, np.array([np.quantile(m, i) for i in np.arange(0, 1+0.01, 0.01)]),
+                               np.array([np.mean(sorted(title, )), np.median(sorted(title, )), np.std(sorted(title, ))])))
+    return X
+        
+
 def tf_idf(doc_to_title, train_test_data, train=True, test=False, n_features=20):
     data = pd.read_csv(train_test_data)
     groups = data.groupby('group_id')
     vectors = TfidfVectorizer().fit_transform([doc_to_title[i] for i in range(len(doc_to_title))])
     
     if train:
-        X = np.empty(shape=(data.shape[0], n_features), dtype=np.float)
+        X = np.empty(shape=(data.shape[0], n_features*2+104), dtype=np.float)
         y = np.empty(shape=(data.shape[0], ), dtype=bool)
         group_ids = np.empty(shape=(data.shape[0], ), dtype=int)
 
@@ -79,12 +94,12 @@ def tf_idf(doc_to_title, train_test_data, train=True, test=False, n_features=20)
             group = data.iloc[group_indx]
             group_ids[i:j] = group_id
             y[i:j] = group.target
-            X[i:j] = cosine(group.doc_id, n_features, vectors)
+            X[i:j] = cosine_add_feat(group.doc_id, n_features, vectors)
             i = j
 
         return X, y, group_ids
     else:
-        X = np.empty(shape=(data.shape[0], n_features), dtype=np.float)
+        X = np.empty(shape=(data.shape[0], n_features*2+104), dtype=np.float)
         pair_ids = np.empty(shape=(data.shape[0], ), dtype=int)
 
         i = 0
@@ -92,7 +107,7 @@ def tf_idf(doc_to_title, train_test_data, train=True, test=False, n_features=20)
             j = i + group_indx.size
             group = data.iloc[group_indx]
             pair_ids[i:j] = group.pair_id
-            X[i:j] = cosine(group.doc_id, n_features, vectors)
+            X[i:j] = cosine_add_feat(group.doc_id, n_features, vectors)
             i = j
 
         return X, pair_ids
@@ -109,6 +124,7 @@ def best_th(cls, X, y, groups, k=5):
 
     best = np.argmax(result)
     return space[best], result[best] / k
+ 
     
 def train_classifier(clf, doc_to_title, train_data, n_features=15, k=5, train=True, test=False, scaler_I=False):
     print('preparing tf-idf matrices....')
@@ -122,7 +138,9 @@ def train_classifier(clf, doc_to_title, train_data, n_features=15, k=5, train=Tr
         scaler.fit(X)
         X = scaler.transform(X)
     print('calculating scores....')
+
     threshold, f1 = best_th(clf, X, y, group_ids, k)
+
     print('\nthreshold:', threshold, '\nf1:', f1)
     return threshold, f1
     
@@ -145,4 +163,19 @@ def predict_test(clf, th, doc_to_title, train_data, test_data, n_features=15, tr
     predict = clf.predict(X_test) > th
     return predict
     
-    
+def tune(clf, param_grid, doc_to_title, train_file):
+    keys = param_grid.keys()
+    values = (param_grid[key] for key in keys)
+    combinations = [dict(zip(keys, combination)) for combination in product(*values)]
+    f_max = -100
+    best_clf = None
+    for comb in tqdm_notebook(combinations):
+        print(comb)
+        try:
+            th, f = train_classifier(clf,  doc_to_title, train_file)
+            if f > f_max:
+                f_max = f
+                best_clf = comb
+        except:
+            continue
+    return f_max, best_clf   
